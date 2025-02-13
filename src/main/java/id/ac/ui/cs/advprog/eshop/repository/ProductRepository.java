3083d39b870b95a870a9f5219ce51644e161f716
Kevin Manurung <kevinmanurung0207@gmail.com>
2025-02-13 21:25:49 +0700

    Fitur edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,37 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.UUID;
 @Repository
 public class ProductRepository {
     private List<Product> productsData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null || product.getProductId().isEmpty()) {
+            product.setProductId(UUID.randomUUID().toString()); // Set ID jika kosong
+        }
         productsData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productsData.iterator();
+    }
+
+    public Product findById(String productId) {
+        return productsData.stream()
+                .filter(product -> product.getProductId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productsData.size(); i++) {
+            if (productsData.get(i).getProductId().equals(updatedProduct.getProductId())) {
+                productsData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
     }
 }
