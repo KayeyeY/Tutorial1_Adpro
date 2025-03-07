b01ac125b6f35f908a789df878fa261516e72fa8
Kevin Manurung <kevinmanurung0207@gmail.com>
2025-03-07 16:48:17 +0700

    [RED] test for payment

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -1,5 +1,5 @@
 package id.ac.ui.cs.advprog.eshop.repository;
-import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
